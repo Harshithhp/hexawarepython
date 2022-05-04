0d69d8ed59319c1c369bfc7b6ce1f65c125f1389
Harshithhp <93958403+Harshithhp@users.noreply.github.com>
2022-05-04 20:37:46 +0530

    Add files via upload
    day2

---
+++ 02-05-22/p1.py
@@ -0,0 +1,76 @@
+name="hp"
+print(type(name));
+
+
+print("========");
+
+
+a=b=c=101;
+print(a);
+print(b);
+print(c);
+
+
+
+
+print("=====================================================================");
+x=10;
+y=200;
+if x==100 and y == 200:
+    print("hii");
+else:
+    print("hellooo");
+
+
+print("===================================================================================");
+
+'''
+username = input("Enter username :");
+passw = int (input("Enter password :"));
+if username == "xyz" and passw == 123:
+    print("hii" ,usernmae);
+else:
+    print("invalid"); 
+'''
+
+
+
+print("===================================================================================");
+
+
+'''
+
+mr = int(input("Enter range :"));
+for i in range(mr):
+    if(i%2==0):
+        print(i, end = "  ");
+        
+    if(i%2!=0):
+         print(i);
+
+
+
+for i in range(20):
+    if(i==5):
+        print("Hello" ,i)
+        break;
+    print(" ");
+    continue;
+    print("Hi ",i)
+
+
+mr=input("Enter name :")
+if mr=="HP":
+    pass;
+else:
+    print("I U :");
+  
+'''    
+
+
+i=1;
+while(i<=10):
+    print("While loop :",i);
+    i+=1;
+
+       
